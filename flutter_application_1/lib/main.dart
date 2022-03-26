2658f434949e56797f7b8986d0de5ff59bb5632f
Jiaz02 <jsaavedraa@iesch.org>
2022-03-26 23:30:29 +0100

    cambios

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
+// creamos un widget  con el nombre que pone en el runApp
+// normalmente statelessWidget
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+        //creamos el home
+        //hacemos wrap with centre en el text para meterlo dentro del Center
+        home: Center(
+      //le ponemos un text simple
+      child: Text('Hola Mundo'),
+    ));
   }
 }
