db7fd8a95e424c32eee952abcfda51712b0b16a7
Jiaz02 <jsaavedraa@iesch.org>
2022-03-27 20:49:59 +0200

    añadimos el boton flotando

--- flutter_application_1/lib/screens/home_screen.dart
+++ flutter_application_1/lib/screens/home_screen.dart
@@ -8,6 +8,9 @@ class HomeScreen extends StatelessWidget {
     //hacemos una variable guardar el stilo
     const TextStyle fontSize30 = TextStyle(fontSize: 30);
     const TextStyle fontSize20 = TextStyle(fontSize: 20);
+
+    int num = 0;
+
     //creamos un scaffold
     return Scaffold(
       //creamos un appbar de un tamaño x
@@ -27,12 +30,25 @@ class HomeScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           //centramos la columna con CrossAxisAlignment en eje x respecto al item mas grande
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: const <Widget>[
+          children: <Widget>[
             //cambiamos las propiedades del texto
-            Text('Contador de clicks', style: fontSize30),
-            Text('10', style: fontSize20)
+            const Text('Contador de clicks', style: fontSize30),
+            Text('$num', style: fontSize20)
           ],
         ),
+      ),
+      //asignamos la ubicacion del floating action button
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterFloat,
+      //creamos el floating action button
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.purple,
+        elevation: 10.0,
+        child: const Icon(Icons.add),
+        onPressed: () {
+          num++;
+          print(num);
+        },
       ),
     );
   }
