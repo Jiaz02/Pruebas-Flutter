9db1bc2d7585e1077d541067c58db5d0e2887176
Jiaz02 <jsaavedraa@iesch.org>
2022-03-27 20:24:25 +0200

    centrar las cosas de la columna

--- flutter_application_1/lib/screens/home_screen.dart
+++ flutter_application_1/lib/screens/home_screen.dart
@@ -6,13 +6,26 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //creamos un scaffold
-    return const Scaffold(
-      //ponemos color dde fondo
-      backgroundColor: Colors.green,
+    return Scaffold(
+      //creamos un appbar de un tamaño x
+      appBar: AppBar(
+        //color del appbar
+        backgroundColor: Colors.green,
+        //texto del appbar
+        title: const Center(child: Text('Holis')),
+        //sombrita de abajo
+        elevation: 10.0,
+      ),
+      //ponemos color de fondo
+      backgroundColor: Colors.greenAccent,
       body: Center(
-        //le ponemos un text simple
-        //hacemos wrap with centre en el text para meterlo dentro del Center
-        child: Text('HomeScreen'),
+        child: Column(
+          //centramos la columna con mainAxisAligment en eje y
+          mainAxisAlignment: MainAxisAlignment.center,
+          //centramos la columna con CrossAxisAlignment en eje x respecto al item mas grande
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const <Widget>[Text('Contador de clicks'), Text('10')],
+        ),
       ),
     );
   }
